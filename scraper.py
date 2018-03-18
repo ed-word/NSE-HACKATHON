54920457e14fec89c78c5ea4b834ad36c42ec17b
Saumitra Bose <imtoobose@gmail.com>
2018-03-18 06:08:32 +0530

    changes

--- scraper.py
+++
@@ -1,3 +0,0 @@
-import requests
-//*[@id="mc_mainWrapper"]/div[3]/div[2]/div[3]/div[2]/div[2]/div/div[3]/div[1]/div[1]/div[2]/a[1]
-//*[@id="mc_mainWrapper"]/div[3]/div[2]/div[3]/div[2]/div[2]/div/div[3]/div[1]/div[2]/div[2]/a[1]
